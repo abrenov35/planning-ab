5c49be91d9c7738c165fc5edb90378b8cdb2a821
abrenov35 <abrenov35@gmail.com>
2026-09-15 19:16:01 +0200

    Neutraliser les anciennes suppressions locales (v179)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './free-affectation-colors.css';
 
+// Neutralise l'ancienne file locale de suppressions AVANT de charger App/api.js.
+// Une file conservée dans un navigateur pouvait rejouer une suppression ancienne
+// et effacer une affectation recréée depuis un autre poste.
+try {
+  localStorage.removeItem('abPlanningDeleteQueueV1');
+} catch (_) {}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+import('./App').then(({ default: App }) => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
